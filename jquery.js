5b811ea9573eae4cf669a348b44b31622196335c
Taras101 <taras101@gmail.com>
2014-02-18 22:14:33 -0500

    added some jquery

---
+++ jquery.js
@@ -0,0 +1,19 @@
+
+ $(document).ready(function() {
+
+  $("h2").fadeOut(1500);
+	$("h2").fadeIn(1000);
+	$("span").delay(2500).fadeOut(600);
+	$("span").fadeIn(800);
+	$("pre").fadeOut(2000);
+	$("pre").fadeIn(1000);
+  $("h3").fadeOut(2000);
+  $("h3").fadeIn(1000);
+});
+
+
+
+
+
+
+
