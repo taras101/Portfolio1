715697bd3d941b376ee1f8c6881f5bdf2c6acfe4
Taras101 <taras101@gmail.com>
2014-03-26 21:08:43 -0400

    cleaned js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,24 +24,28 @@
 	$("pre").fadeOut(2000).fadeIn(1000);
   $("h3").fadeOut(2000).fadeIn(1000);
   moveSpk(3000);
-	var $test = $('.bio');
-	var initText = $.trim($test.text()), ptr = 0;
-	var timer = setInterval(function() {
-	    var ln = $.trim($test.find('.trans').text().length);
-	      if (ln == initText.length) {
-	          $test.empty();
-	          clearInterval(timer);
-	      }
 
-	      $('.bio').html(function() {
+$(function() {
 
-	          return $('<span>').addClass('removeMe')
-	              .html(initText.substring(ptr++ , ptr))
-	              .before($('<span>').addClass('trans').
-	                     html(initText.substring(0 , ptr-1)))
-	              .after(initText.substring(ptr));
-	      }).find('span.removeMe').animate({'opacity': 0}, 200);
 
-	  }, 300);
+    var $test = $('#test');
+    var initText = $.trim($test.text()), ptr = 0;
+    var timer = setInterval(function() {
+        var ln = $.trim($test.find('.trans').text().length);
+        if (ln == initText.length) {
+            $test.empty();
+            clearInterval(timer);
+        }
 
+        $('#test').html(function() {
+
+            return $('<span>').addClass('removeMe')
+                .html(initText.substring(ptr++ , ptr))
+                .before($('<span>').addClass('trans').
+                       html(initText.substring(0 , ptr-1)))
+                .after(initText.substring(ptr));
+        }).find('span.removeMe').animate({'opacity': 0}, 200);
+
+    }, 300);
+});
 });
