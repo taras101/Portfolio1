3efbf42166f4c20912829be3da61915ae9970804
Taras101 <taras101@gmail.com>
2014-04-10 14:50:56 -0400

    working on string eating

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,19 +16,9 @@
 //= require_tree .
 
  $(document).ready(function() {
-	function moveSpk(delay){
-	$(".spk")[0].setAttribute("src", "https://s3-us-west-2.amazonaws.com/s1portfolio/images/animatedspk2.gif")
-	};
-  $("h2").fadeOut(1500).fadeIn(1000);
-	$("span").delay(2500).fadeOut(600).fadeIn(800);
-	$("pre").fadeOut(2000).fadeIn(1000);
-  $("h3").fadeOut(2000).fadeIn(1000);
-  moveSpk(3000);
 
 $(function() {
-
-
-    var $test = $('#test');
+    var $test = $('#burn');
     var initText = $.trim($test.text()), ptr = 0;
     var timer = setInterval(function() {
         var ln = $.trim($test.find('.trans').text().length);
@@ -37,7 +27,7 @@ $(function() {
             clearInterval(timer);
         }
 
-        $('#test').html(function() {
+        $('#burn').html(function() {
 
             return $('<span>').addClass('removeMe')
                 .html(initText.substring(ptr++ , ptr))
---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,11 @@
+//  $(document).ready(function() {
+
+// function moveSpk(delay){
+// -   $(".spk")[0].setAttribute("src", "https://s3-us-west-2.amazonaws.com/s1portfolio/images/animatedspk2.gif")};
+// -  $("h2").fadeOut(1500).fadeIn(1000);
+// -   $("span").delay(2500).fadeOut(600).fadeIn(800);
+// -   $("pre").fadeOut(2000).fadeIn(1000);
+// -  $("h3").fadeOut(2000).fadeIn(1000);
+// -  moveSpk(3000);
+
+// });
